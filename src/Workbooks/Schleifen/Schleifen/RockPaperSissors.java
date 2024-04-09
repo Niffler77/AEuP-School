c14601ced917f89ca03ef28013cdfe452fbdfec8
Niffler77 <niffler77@gmx.net>
2024-04-09 08:19:13 +0200

    Build Fix

--- src/Workbooks/Schleifen/Schleifen/RockPaperSissors.java
+++ src/Workbooks/Schleifen/Schleifen/RockPaperSissors.java
@@ -1,6 +1,8 @@
+package Workbooks.Schleifen.Schleifen;
+
 import java.util.Scanner;
 
-public class Main {
+public class RockPaperSissors {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
