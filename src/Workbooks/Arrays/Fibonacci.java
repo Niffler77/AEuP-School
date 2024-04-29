ffe3c5f5b3acad539ae1ff7ea640f6a403300cec
Lasse.Ruhkieck <lasse.ruhkieck@scaltel.de>
2024-04-29 08:35:11 +0200

    Fibonacci Beispiel

---
+++ src/Workbooks/Arrays/Fibonacci.java
@@ -0,0 +1,20 @@
+package Workbooks.Arrays;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+
+        System.out.println("Create a Fibonacci-Sequence with a array");
+
+        long[] fibonacci = new long[50];
+        fibonacci [0] = 0;
+        fibonacci [1] = 1;
+
+        for (int i = 2; i < fibonacci.length; i++) {
+            fibonacci[i] = fibonacci [i - 1] + fibonacci [i - 2];
+        }
+
+        for (long l : fibonacci) {
+            System.out.println(l);
+        }
+    }
+}
