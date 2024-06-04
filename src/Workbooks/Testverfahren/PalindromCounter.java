b352db89e91bb2e4594d823f6c8eac0945a007dc
Niffler77 <99284934+Niffler77@users.noreply.github.com>
2024-06-04 09:58:43 +0200

    Imported Palindromcounter.java from Teams

---
+++ src/Workbooks/Testverfahren/PalindromCounter.java
@@ -0,0 +1,28 @@
+package Workbooks.Testverfahren;
+
+public class PalindromCounter {
+
+    public static void main(String[] args)
+    {
+        String[] words = {"radar", "Level", "world", "deified", "civic", "example"};
+        int palindromeCount = 0;
+
+        for (int i = 0; i < words.length; i++)
+        {
+            String word = words[i];
+            String reversed = "";
+
+            for (int j = word.length() - 1; j >= 0; j--)
+            {
+                reversed += word.charAt(j);
+            }
+
+            if (word.equals(reversed)) {
+                palindromeCount++;
+            }
+        }
+
+        System.out.println("Anzahl der Palindrome: " + palindromeCount);
+    }
+
+}
