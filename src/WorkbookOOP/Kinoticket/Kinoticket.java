f51ae3fef8a8152e038f60756cc48c9b2d83cb83
Niffler77 <99284934+Niffler77@users.noreply.github.com>
2024-06-24 08:53:19 +0200

    Object Orientierung Einstieg rest

--- src/WorkbookOOP/Kinoticket/Kinoticket.java
+++ src/WorkbookOOP/Kinoticket/Kinoticket.java
@@ -10,6 +10,17 @@ public class Kinoticket {
 
     boolean is3d;
 
+    public Kinoticket() {
+
+    }
+
+    public Kinoticket(String tilte, int ticketnr, float price, boolean is3d) {
+        this.tilte = tilte;
+        this.ticketnr = ticketnr;
+        this.price = price;
+        this.is3d = is3d;
+    }
+
     public void printTicket() {
         System.out.println("Title: " + tilte + " Ticketnr: " + ticketnr + " Price: " + price + " Is3d: " + is3d);
     }
