9d19b28c25d67d22f6fab69e4a792a6d131c574c
David Bach <db@medienpalast.net>
2024-06-25 10:32:34 +0200

    fix fix

---
+++ src/Workbooks/OOP/Auto/David/AutoTest.java
@@ -0,0 +1,12 @@
+package Workbooks.OOP.Auto.David;
+
+public class AutoTest {
+    public static void main(String[] args) {
+        Auto meinBmw = new Auto();
+        meinBmw.marke = "BMW";
+        meinBmw.type = "320d";
+        meinBmw.farbe = "schwarz";
+        meinBmw.ausgabe();
+
+    }
+}
