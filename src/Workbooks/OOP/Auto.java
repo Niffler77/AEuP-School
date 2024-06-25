09c02454d433cfde4619b7fd358863a56cab6957
Jakob Joas <jakob.joas@fideliushaus.com>
2024-06-25 09:26:20 +0200

    + Auto Klasse hinzugefügt

---
+++ src/Workbooks/OOP/Auto.java
@@ -0,0 +1,14 @@
+package Workbooks.OOP;
+
+public class Auto {
+    public String farbe = "";
+    public String marke = "";
+    public String type = "";
+    public int number;
+
+    public void ausgabe() {
+        System.out.println("Die Marke ist: " + marke);
+        System.out.println("Der Typ ist: " + type);
+    }
+
+}
