caf9c5c8e5357beb1fad2a33064dc063a925c37e
Jakob Joas <jakob.joas@fideliushaus.com>
2024-06-25 10:45:06 +0200

    ~ Added constructors to Auto class

--- src/Workbooks/OOP/Auto/Jakob/Auto.java
+++ src/Workbooks/OOP/Auto/Jakob/Auto.java
@@ -6,7 +6,31 @@ public class Auto {
     public String type = "";
     public int number;
 
-    public void ausgabe() {
+    public Auto ()
+    {
+        // Empty constructor
+    }
+
+    public Auto(String marke)
+    {
+        this.marke = marke;
+    }
+
+    public Auto(String marke, String type)
+    {
+        this.marke = marke;
+        this.type = type;
+    }
+
+    public Auto(String marke, String typ, String farbe)
+    {
+        this.marke = marke;
+        this.type = typ;
+        this.farbe = farbe;
+    }
+
+    public void ausgabe()
+    {
         System.out.println("Die Marke ist: " + marke);
         System.out.println("Der Typ ist: " + type);
     }
--- src/Workbooks/OOP/Auto/Jakob/AutoTest.java
+++ src/Workbooks/OOP/Auto/Jakob/AutoTest.java
@@ -2,11 +2,12 @@ package Workbooks.OOP.Auto.Jakob;
 
 public class AutoTest {
     public static void main(String[] args) {
-        Auto meinBmw = new Auto();
+        Auto meinBmw = new Auto(); // uses empty constructor
         meinBmw.marke = "BMW";
         meinBmw.type = "320d";
         meinBmw.farbe = "schwarz";
         meinBmw.ausgabe();
 
+        Auto meinVW = new Auto("VW", "Golf", "blau");
     }
 }
