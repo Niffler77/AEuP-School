a3e4edb40147a07dba6c33562ab8ee73b8fe4fa0
JannisMiniMee <lasse.ruhkieck@scaltel.de>
2024-06-25 09:33:01 +0200

    Fertigstellung der Funktion

--- src/WorkbookOOP/Kinoticket/AutoÜbung/Auto.java
+++ src/WorkbookOOP/Kinoticket/AutoÜbung/Auto.java
@@ -1,4 +1,14 @@
 package WorkbookOOP.Kinoticket.AutoÜbung;
 
 public class Auto {
+
+    public String farbe = "";
+    public String marke = "";
+    public String typ = "";
+    public int nummer;
+
+    public void ausgabe() {
+        System.out.println("Die Marke ist: " + marke);
+        System.out.println("Der Typ ist: " + typ);
+    }
 }
--- src/WorkbookOOP/Kinoticket/AutoÜbung/AutoTest.java
+++ src/WorkbookOOP/Kinoticket/AutoÜbung/AutoTest.java
@@ -4,6 +4,9 @@ public class AutoTest {
 
     public static void main(String[] args) {
         Auto meinBmw = new Auto();
-
+        meinBmw.marke = "BMW";
+        meinBmw.farbe = "Blau";
+        meinBmw.typ = "E36";
+        meinBmw.ausgabe();
     }
 }
