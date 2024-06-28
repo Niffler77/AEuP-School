5adbdaa0c341ad1eec948cab5979292958bf1b41
Jakob Joas <jakob.joas@fideliushaus.com>
2024-06-28 12:14:33 +0200

    + Added Sparbuch Task

--- src/Workbooks/OOP/Sparbuch/Jakob/Sparbuch.java
+++ src/Workbooks/OOP/Sparbuch/Jakob/Sparbuch.java
@@ -1,4 +1,15 @@
 package Workbooks.OOP.Sparbuch.Jakob;
 
 public class Sparbuch {
+    public double kontostand = 0.0;
+    public String name;
+    public double zinssatz = 0.0;
+
+    public void buchen(double betrag) {
+        kontostand += betrag;
+    }
+
+    public double zinsenBerechnen() {
+        return kontostand * zinssatz;
+    }
 }
